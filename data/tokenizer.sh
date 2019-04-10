4803100763af650b8f6d339b0fca757f9988dc39
chung <chung.yang@engineer.uconn.edu>
2019-04-09 20:50:03 -0400

    add tokenizer script back

---
+++ data/tokenizer.sh
@@ -0,0 +1,2 @@
+for l in en de; do for f in data/*.$l; do if [[ "$f" != *"test"* ]]; then sed -i "$ d" $f; fi;  done; done
+for l in en de; do for f in data/*.$l; do perl tokenizer.perl -a -no-escape -l $l -q  < $f > $f.atok; done; done
