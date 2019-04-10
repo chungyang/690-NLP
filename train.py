6d0560e3738af9455674bd1aac9886f262e4b0bd
chung <chung.yang@engineer.uconn.edu>
2019-04-09 20:55:03 -0400

    working on general training procedure

---
+++ train.py
@@ -0,0 +1,40 @@
+import argparse
+import torch
+
+def main():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-data', required=True)
+
+    parser.add_argument('-epoch', type=int, default=10)
+    parser.add_argument('-batch_size', type=int, default=64)
+
+    parser.add_argument('-d_model', type=int, default=512)
+    parser.add_argument('-d_inner_hid', type=int, default=2048)
+    parser.add_argument('-d_k', type=int, default=64)
+    parser.add_argument('-d_v', type=int, default=64)
+
+    parser.add_argument('-n_head', type=int, default=8)
+    parser.add_argument('-n_layers', type=int, default=6)
+    parser.add_argument('-n_warmup_steps', type=int, default=4000)
+
+    parser.add_argument('-dropout', type=float, default=0.1)
+    parser.add_argument('-embs_share_weight', action='store_true')
+    parser.add_argument('-proj_share_weight', action='store_true')
+
+    parser.add_argument('-log', default=None)
+    parser.add_argument('-save_model', default=None)
+    parser.add_argument('-save_mode', type=str, choices=['all', 'best'], default='best')
+
+    parser.add_argument('-cuda', action='store_true')
+    parser.add_argument('-label_smoothing', action='store_true')
+
+    options = parser.parse_args()
+
+    # Set device type
+    device = torch.device("cuda" if options.cuda else "cpu")
+
+
+if __name__ == "__main__":
+    main()
