5b0386163b42f45ca89908413ec5c569216d4163
chung <chung.yang@engineer.uconn.edu>
2019-04-09 20:46:40 -0400

    finish preprocess

--- Preprocess.py
+++ Preprocess.py
@@ -1,5 +1,6 @@
 import argparse
 import Tags
+import torch
 
 def get_instances(file, max_len):
     """
@@ -53,20 +54,43 @@ def match_size(first_instances, second_instances):
 
     return first_instances, second_instances
 
+def word2index(sentences):
+    """
+    Maps each word in sentences to an unique ID
+
+    :param sentences: lists of words
+    :return: dictionary that maps each word in sentences to an unique ID
+    """
+
+    all_vocab = set(w for sentence in sentences for w in sentence)
+
+    word2idx = {Tags.BOS:Tags.BOS_ID, Tags.EOS:Tags.EOS_ID, Tags.UNK:Tags.UNK_ID, Tags.PAD:Tags.PAD_ID}
+
+    for vocab in all_vocab:
+        word2idx[vocab] = len(word2idx)
+
+    return word2idx
+
+def get_idx_seq(sentences, word2idx):
+    """
+    Replace words in sentences with an ID that's specified in word2idx.
+
+    :param sentences: lists of words
+    :param word2idx: dictionary that maps each word to an unique ID
+    :return: lists of IDs
+    """
+
+    return [[word2idx.get(w, Tags.UNK) for w in s] for s in sentences]
 
 def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-train_src', required=True)
     parser.add_argument('-train_tgt', required=True)
-    # parser.add_argument('-valid_src', required=True)
-    # parser.add_argument('-valid_tgt', required=True)
-    # parser.add_argument('-save_data', required=True)
+    parser.add_argument('-dev_src', required=True)
+    parser.add_argument('-dev_tgt', required=True)
+    parser.add_argument('-save_data', required=True)
     parser.add_argument('-max_len', type=int, default=50)
-    parser.add_argument('-min_word_count', type=int, default=5)
-    parser.add_argument('-keep_case', action='store_true')
-    parser.add_argument('-share_vocab', action='store_true')
-    parser.add_argument('-vocab', default=None)
 
     options = parser.parse_args()
     # Include <BOS> <EOS> tokens
@@ -79,9 +103,34 @@ def main():
     # Make sure src and tgt have the same size
     train_src, train_tgt = match_size(train_src, train_tgt)
 
-    
+    # Get dev instances
+    dev_src = get_instances(options.dev_src, options.max_len)
+    dev_tgt = get_instances(options.dev_tgt, options.max_len)
 
+    # Make sure src and tgt have the same size
+    dev_src, dev_tgt = match_size(dev_src, dev_tgt)
 
+    # Get word2idx for training data
+    src_word2idx = word2index(train_src)
+    tgt_word2idx = word2index(train_tgt)
+
+    # Convert words to IDs
+    train_src_idx_seq = get_idx_seq(train_src, src_word2idx)
+    train_tgt_idx_seq = get_idx_seq(train_tgt, tgt_word2idx)
+
+    dev_src_idx_seq = get_idx_seq(dev_src, src_word2idx)
+    dev_tgt_idx_seq = get_idx_seq(dev_tgt, tgt_word2idx)
+
+    data = {"word2idx":
+                { "src": src_word2idx, "tgt": tgt_word2idx},
+            "train":
+                {"src": train_src_idx_seq, "tgt": train_tgt_idx_seq},
+            "dev":
+                {"src": dev_src_idx_seq, "tgt": dev_tgt_idx_seq}
+            }
+
+    # Save processed data
+    torch.save(data, options.save_data)
 
 
 if __name__ == "__main__":
--- Tags.py
+++ Tags.py
@@ -2,3 +2,8 @@ BOS = "<BOS>"
 EOS = "<EOS>"
 UNK = "<UNK>"
 PAD = "<BLANK>"
+
+BOS_ID = 0
+EOS_ID = 1
+UNK_ID = 2
+PAD_ID = 3
