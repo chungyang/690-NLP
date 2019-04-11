47f0d6bb0cdcc116561561f3653a997a4735ac96
chung <chung.yang@engineer.uconn.edu>
2019-04-11 15:12:40 -0400

    various utility functions such as getting different kinds of masks

---
+++ transformer/utils.py
@@ -0,0 +1,47 @@
+import numpy as np
+import Tags
+import torch
+
+def get_non_pad_mask(seq):
+    assert seq.dim() == 2
+    return seq.ne(Tags.PAD_ID).type(torch.float).unsqueeze(-1)
+
+def get_sine_pos_encoding(n_position, d_hid, padding_idx=None):
+
+    def cal_angle(position, hid_idx):
+        return position / np.power(10000, (2 * hid_idx) / d_hid)
+
+    def get_posi_angle_vec(position):
+        return [cal_angle(position, hid_j) for hid_j in range(d_hid)]
+
+    sinusoid_table = np.array([get_posi_angle_vec(pos_i) for pos_i in range(n_position)])
+
+    sinusoid_table[:, 0::2] = np.sin(sinusoid_table[:, 0::2])  # dim 2i
+    sinusoid_table[:, 1::2] = np.cos(sinusoid_table[:, 1::2])  # dim 2i+1
+
+    if padding_idx is not None:
+        # zero vector for padding dimension
+        sinusoid_table[padding_idx] = 0.
+
+    return torch.FloatTensor(sinusoid_table)
+
+def get_attn_key_pad_mask(seq_k, seq_q):
+    ''' For masking out the padding part of key sequence. '''
+
+    len_q = seq_q.size(1)
+    # Element wise comparison to get a mask for padding
+    padding_mask = seq_k.eq(Tags.PAD_ID)
+    padding_mask = padding_mask.unsqueeze(1).expand(-1, len_q, -1)  # b x lq x lk
+
+    return padding_mask
+
+def get_subsequent_mask(seq):
+    ''' For masking out the subsequent info. '''
+
+    sz_b, len_s = seq.size()
+    subsequent_mask = torch.triu(
+        torch.ones((len_s, len_s), device=seq.device, dtype=torch.uint8), diagonal=1)
+    subsequent_mask = subsequent_mask.unsqueeze(0).expand(sz_b, -1, -1)  # b x ls x ls
+
+    return subsequent_mask
+
